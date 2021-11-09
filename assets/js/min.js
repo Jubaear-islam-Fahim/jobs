5a86917867220e1d00d0ef4356961c00308f7f2e
Jubaear <jubaearislam86664@gmail.com>
2021-11-09 17:24:06 +0600

    candidates style

--- assets/js/min.js
+++ assets/js/min.js
@@ -1,39 +1,87 @@
 (function ($) {
     'use strict'
- 
-        var a = 0;
-        var oTop = $('#counter').offset().top - window.innerHeight;
-        console.log($('#counter').offset().top);
-        if (a == 0 && $(window).scrollTop() > oTop) {
-            $('.counter-value').each(function () {
-                var $this = $(this),
-                    countTo = $this.attr('data-count');
-                $({
-                    countNum: $this.text()
-                }).animate({
-                        countNum: countTo
+
+
+
+    // Counter 
+    var a = 0;
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    console.log($('#counter').offset().top);
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter-value').each(function () {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                    countNum: countTo
+                },
+
+                {
+
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
                     },
+                    complete: function () {
+                        $this.text(this.countNum);
+                        //alert('finished');
+                    }
 
-                    {
+                });
+        });
+        a = 1;
+    }
 
-                        duration: 2000,
-                        easing: 'swing',
-                        step: function () {
-                            $this.text(Math.floor(this.countNum));
-                        },
-                        complete: function () {
-                            $this.text(this.countNum);
-                            //alert('finished');
-                        }
+    // Range Slider
+    const allRanges = document.querySelectorAll(".range-wrap");
+    allRanges.forEach(wrap => {
+        const range = wrap.querySelector(".range");
+        const bubble = wrap.querySelector(".bubble");
 
-                    });
-            });
-            a = 1;
-        }
- 
+        range.addEventListener("input", () => {
+            setBubble(range, bubble);
+        });
+        setBubble(range, bubble);
+    });
+
+    function setBubble(range, bubble) {
+        const val = range.value;
+        const min = range.min ? range.min : 0;
+        const max = range.max ? range.max : 100;
+        const newVal = Number(((val - min) * 100) / (max - min));
+        bubble.innerHTML = val;
+
+        // Sorta magic numbers based on size of the native UI thumb
+        bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+    }
 
 
 
+    $('#container').imagesLoaded(function() {
+        var $grid = $('.candidates-grid').isotope({
+            itemSelector: '.candidates-grid-item',
+            percentPosition: true,
+            masonry: {
+                // use outer width of grid-sizer for columnWidth
+                columnWidth: '.candidates-grid-item',
+            }
+        });
+
+        // filter items on li click
+        $('.candidates-filter').on('click', 'li', function() {
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({ filter: filterValue });
+        });
+
+        //for menu active class
+        $('.candidates-filter li').on('click', function(event) {
+            $(this).siblings('.active').removeClass('active');
+            $(this).addClass('active');
+            event.preventDefault();
+        });
+    });
 
 
 })(jQuery);
